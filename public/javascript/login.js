514971b2fdb801348b20fa8c0e8e395ab822c02e
bjackels5 <85116312+bjackels5@users.noreply.github.com>
2021-09-23 20:33:34 -0400

    took unused signup code out of login.js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,28 +1,28 @@
-async function signupFormHandler(event) {
-    event.preventDefault();
+// async function signupFormHandler(event) {
+//     event.preventDefault();
 
-    const username = document.querySelector("#username").value.trim();
-    const password = document.querySelector("#password").value.trim();
+//     const username = document.querySelector("#username").value.trim();
+//     const password = document.querySelector("#password").value.trim();
 
-    if (username && password) {
-        const response = await fetch('/api/users', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
+//     if (username && password) {
+//         const response = await fetch('/api/users', {
+//             method: 'post',
+//             body: JSON.stringify({
+//                 username,
+//                 password
+//             }),
+//             headers: { 'Content-Type': 'application/json' }
+//         });
         
-        // check the response status
-        if (response.ok) {
-            console.log('success');
-            document.location.replace('/'); 
-        } else {
-            alert(response.statusText);
-        }
-    }
-}
+//         // check the response status
+//         if (response.ok) {
+//             console.log('success');
+//             document.location.replace('/'); 
+//         } else {
+//             alert(response.statusText);
+//         }
+//     }
+// }
 
 async function loginFormHandler(event) {
 
@@ -51,13 +51,14 @@ async function loginFormHandler(event) {
     }
 }
 
-const signupFormEl = document.querySelector('.signup-form');
-if (signupFormEl) {
-    signupFormEl.addEventListener('submit', signupFormHandler);
-}
+// const signupFormEl = document.querySelector('.signup-form');
+// if (signupFormEl) {
+//     signupFormEl.addEventListener('submit', signupFormHandler);
+// }
 
-const loginFormEl = document.querySelector('.login-form');
-if (loginFormEl) {
-    loginFormEl.addEventListener('submit', loginFormHandler);
-}
+// const loginFormEl = document.querySelector('.login-form');
+// if (loginFormEl) {
+//     loginFormEl.addEventListener('submit', loginFormHandler);
+// }
 
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
